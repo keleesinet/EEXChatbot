1d55d67bd8f35a4f7aa7d8eecaf992d3fd9b8c18
Kelee Jone Sinet <kelee.jone.t.sinet@accenture.com>
2018-04-13 15:41:11 +0800

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow: {
+    angularBot: '21ccf1b5d3a84991b6314de930f6a99b'
+  }
 };
